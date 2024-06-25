26eb9d8782a8cca35b966bc1141d53a6e31d3a83
Youmin Kim <powereyes@gmail.com>
2024-06-25 22:45:27 +0800

    image size

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
 import Markdown from "markdown-to-jsx";
+import Image from "next/image";
 import { getBook } from "./utils/books";
 
 export default function Home() {
   const home = getBook("home");
-  return <Markdown>{home.content}</Markdown>;
+  return (
+    <Markdown
+      options={{
+        overrides: {
+          img: {
+            component: (props) => (
+              <Image
+                alt={"img"}
+                {...props}
+                width={400}
+                height={400}
+                style={{
+                  objectFit: "contain",
+                }}
+              />
+            ),
+          },
+        },
+      }}
+    >
+      {home.content}
+    </Markdown>
+  );
 }
